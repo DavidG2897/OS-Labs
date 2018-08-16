0345b2c23031b5cf15c917f7b5796d00163db41a
oscreader <oscreader@OSC>
2018-08-15 21:35:41 -0600

    lab 1.4

---
+++ lab1.4/.tmp_versions/simple.mod
@@ -0,0 +1,2 @@
+/home/oscreader/repos/OS-Labs/lab1.4/simple.ko
+/home/oscreader/repos/OS-Labs/lab1.4/simple.o
--- lab1.4/simple.c
+++ lab1.4/simple.c
@@ -1,18 +1,37 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/jiffies.h>
+#include <linux/utsname.h>
+
+void get_time(void);
+void getSystemInfo(void);
 
 /* This function is called when the module is loaded. */
-int simple_init(void)
-{
-       printk(KERN_INFO "Loading Module\n");
-
-       return 0;
+int simple_init(void) {
+       	printk(KERN_INFO "Initialization\n");
+	get_time();
+	getSystemInfo();
+       	return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+	printk(KERN_INFO "Termination\n");
+}
+
+void get_time(void) {
+	printk("Uptime: %d seconds\n",jiffies_to_msecs(get_jiffies_64())/1000);
+}
+
+void getSystemInfo(void){
+	struct new_utsname *info=utsname();
+	printk("Sysname: %s\n",info->sysname);
+	printk("Release: %s\n",info->release);
+	printk("Nodename: %s\n",info->nodename);
+	printk("Version: %s\n",info->version);
+	printk("Machine: %s\n",info->machine);
+
 }
 
 /* Macros for registering module entry and exit points. */
