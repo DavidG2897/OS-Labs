44c42ba775cdef1f6c4b2219e9cfd5e07d289530
oscreader <oscreader@OSC>
2018-08-28 06:28:03 -0600

    lab 1.5

--- lab1.5/script.sh
+++ lab1.5/script.sh
@@ -3,12 +3,10 @@
 D='/usr/src'
 output_file='/home/oscreader/repos/OS-Labs/lab1.5/linux_source_structure.txt'
 
-for d in $D; do
-	if [ -d "$d" ]; then
-		echo 'Directory: '$d >> $output_file
-		echo '.c files: ' >> $output_file
-		find $d -name '*.c' >> $output_file
-		echo '.h files: ' >> $output_file
-		find $d -name '*.h' >> $output_file
-	fi
+for d in $( find /usr/src -type d ); do
+	echo 'Directory: '$d >> $output_file
+	echo '.c files: ' >> $output_file
+	ls $d/*.c >> $output_file
+	echo '.h files: ' >> $output_file
+	ls $d/*.h >> $output_file
 done
