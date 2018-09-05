7d644b9e206ed049cc4f9ef9d0d25be53c8be568
oscreader <oscreader@OSC>
2018-09-04 23:47:24 -0600

    Added lab 1.7

---
+++ lab1.7/main.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define TIME_SLICE 10
+#define TASKS_LIMIT 10
+
+enum STATES {IDLE,READY,RUN};		//Ignore WAIT state, no IO
+typedef struct{
+	unsigned char id;
+	unsigned long burst_time;
+	unsigned char priority;
+	unsigned long activation_time;	//Activation time, could get it from system but it will always be sorted
+	enum STATES state;
+} Task;
+Task tasks[TASKS_LIMIT];
+
+void sort(Task list[],unsigned char size,unsigned char policy){
+	Task greater, lesser;
+	unsigned char i,j;
+	Task sorted[size];
+	for(i=0;i<size;i++){
+		sorted[i]=list[i];
+	}
+	for(i=0;i<size;i++){
+		list[i]=sorted[i];
+		for(j=i;j>0;j--){
+			switch(policy){
+				case 1:
+					if(list[j].activation_time<list[j-1].activation_time){
+						greater=list[j-1];
+						lesser=list[j];
+						list[j]=greater;
+						list[j-1]=lesser;
+					}
+					break;
+				case 2:
+					if(list[j].burst_time<list[j-1].burst_time){
+						greater=list[j-1];
+						lesser=list[j];
+						list[j]=greater;
+						list[j-1]=lesser;
+					}
+					break;
+				case 3:
+					if(list[j].priority<list[j-1].priority){
+						greater=list[j-1];
+						lesser=list[j];
+						list[j]=greater;
+						list[j-1]=lesser;
+					}
+					break;
+				case 4:
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
+
+void fcfs(){
+	printf("-------------------FCFS Scheduling-------------------------\n");
+	unsigned char i,first=0;//Assume first task in sorted array is in READY state
+	sort(tasks,TASKS_LIMIT,1);
+	for(i=0;i<TASKS_LIMIT;i++){
+		printf("Task ID: %d ACTIVATION_TIME: %d STATE: %s\n",tasks[i].id,tasks[i].activation_time,(!tasks[i].state) ? "IDLE" : "READY");
+	}
+	for(i=0;i<TASKS_LIMIT;i++){
+		if(tasks[i].state==READY){//Already sorted by activation time, first READY task is selected to RUN
+			first=i;
+			break;	
+		}
+	}
+	printf("-----------------------------------------------------------\n");
+	tasks[first].state=RUN;
+	printf("FCFS scheduling result:\nID: %d ACTIVATION_TIME: %d STATE: RUN\n",tasks[first].id,tasks[first].activation_time);
+}
+
+void priority_scheduling(){
+	printf("-------------------Priority Scheduling-------------------------\n");
+	unsigned char i,first=0;//Assume first task in sorted array is in READY state
+	sort(tasks,TASKS_LIMIT,3);
+	for(i=0;i<TASKS_LIMIT;i++){
+		printf("Task ID: %d PRIORITY: %d STATE: %s\n",tasks[i].id,tasks[i].priority,(!tasks[i].state) ? "IDLE" : "READY");
+	}
+	for(i=TASKS_LIMIT;i>=0;i--){
+		if(tasks[i].state==READY){//Already sorted by piority, first READY task is selected to RUN
+			first=i;
+			break;	
+		}
+	}
+	printf("---------------------------------------------------------------\n");
+	tasks[first].state=RUN;
+	printf("Piority scheduling result:\nID: %d PRIORITY: %d STATE: RUN\n",tasks[first].id,tasks[first].priority);
+}
+
+void init_tasks(unsigned char policy){
+	unsigned char i;
+	switch(policy){
+		case 1:
+			for(i=0;i<TASKS_LIMIT;i++){
+				tasks[i].id=i;
+				tasks[i].state=(rand() % 2); //Randomize autostart
+				if(!tasks[i].state){
+					tasks[i].activation_time=0;
+				}else{
+					tasks[i].activation_time=(rand() % 10000);					
+				}
+			}
+			break;
+		case 2:
+			for(i=0;i<TASKS_LIMIT;i++){
+				tasks[i].id=i;
+				tasks[i].burst_time=(rand() % 50);
+				tasks[i].state=(rand() % 2); //Randomize autostart
+			}
+			break;
+		case 3:
+			for(i=0;i<TASKS_LIMIT;i++){
+				tasks[i].id=i;
+				tasks[i].priority=(rand() % 50);
+				tasks[i].state=(rand() % 2); //Randomize autostart
+			}
+			break;
+		case 4:
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void){
+	srand(time(NULL));
+	unsigned char run=1;
+	while(run){
+		char select;
+		printf("Select scheduling policy:\n1) FCFS\n2) SJF\n3) Priority scheduling\n4) Round-robin scheduling\n>");
+		scanf("%c",&select);
+		switch(select){
+			case '1':
+				init_tasks(1);
+				fcfs();
+				run=0;
+				break;
+			case '2':
+				init_tasks(2);
+				run=0;
+				break;
+			case '3':
+				init_tasks(3);
+				priority_scheduling();
+				run=0;
+				break;
+			case '4':
+				init_tasks(4);
+				run=0;
+				break;
+			default:
+				printf("Please select a valid option\n");
+				break;
+		}
+	}
+	return 0;
+}
