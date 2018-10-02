03a0261a6d801a68b30a19279f077dccf9b10800
david.g.2897@gmail.com <oscreader@OSC>
2018-10-02 07:18:20 -0600

    updated main.c

--- lab4.2/main.c
+++ lab4.2/main.c
@@ -34,47 +34,50 @@ void test(int i){
 	unsigned char left=(i+(CHAIR_COUNT-1))%CHAIR_COUNT;
 	unsigned char right=(i+1)%CHAIR_COUNT;
 	//If philosopher can get both forks...
-	if(THINKING==philosophers[left]==philosophers[i]==philosophers[right]){
+	if((THINKING==philosophers[left])&&(THINKING==philosophers[i])&&(THINKING==philosophers[right])){
 		//.. it starts eating
+		printf("\tPhilosophers %d (%d) and %d (%d) are not eating, ",left,philosophers[left],right,philosophers[right]);
 		philosophers[i]=EATING;
 		pthread_cond_signal(&cond_vars[i]);
 	}
 }
 
 void pickup_forks(int philosopher_number){
-	printf("picking up forks\n");
 	test(philosopher_number);
+	while(EATING!=philosophers[philosopher_number])
+		pthread_cond_wait(&cond_vars[philosopher_number],&mutexes[philosopher_number]);
 }
 
 void return_forks(int philosopher_number){
 	unsigned char left=(philosopher_number+(CHAIR_COUNT-1))%CHAIR_COUNT;
 	unsigned char right=(philosopher_number+1)%CHAIR_COUNT;
+	philosophers[philosopher_number]=THINKING;
+	printf("Philosopher %d finished eating, ", philosopher_number);
+	printf("returned its forks, now thinking (%d)!\n",philosophers[philosopher_number]);
 	test(left);
 	test(right);
 }
 
 void *thread(void *params){
 	int philosopher = (int) params;
-	pthread_mutex_lock(&mutexes[philosopher]);
 	unsigned char left=(philosopher+(CHAIR_COUNT-1))%CHAIR_COUNT;
 	unsigned char right=(philosopher+1)%CHAIR_COUNT;
-	printf("Philosopher %d is now trying to eat...",philosopher);
-	pickup_forks(philosopher);
-	while(EATING!=philosophers[philosopher]){
-		pthread_cond_wait(&cond_vars[philosopher],&mutexes[philosopher]);
-	}
-	printf("\tPhilosophers %d and %d are not eating, %d is eating now!\n",left,right,philosopher);
-	sleep((rand() % 3)+1);		//Eating...
-	philosophers[philosopher]=THINKING;
-	printf("Philosopher %d (%d) finished eating! Returning forks...", philosopher,philosophers[philosopher]);
-	return_forks(philosopher);
-	printf("philosopher %d returned its forks!\n",philosopher);
+	pthread_mutex_lock(&mutexes[philosopher]);
+	do{
+		printf("Philosopher %d is now trying to eat...\n",philosopher);
+		pickup_forks(philosopher);
+		printf("%d (%d) is eating now!\n",philosopher,philosophers[philosopher]);
+		sleep((rand() % 3)+1);		//Eating...
+		return_forks(philosopher);
+		sleep((rand() % 3)+1);		//Thinking...
+	}while(1);
+	pthread_exit(0);
 }
 
 int main(void){
 	srand(SEED);
 	pthread_attr_init(&attr);
 	init_philosophers();
-	printf("All philosophers finished eating!\n");
+	printf("\nAll philosophers finished eating!\n");
 	return 0;
 }
